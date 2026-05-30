1f1ac966c99a5723e76f51e93bc0fcab7da513f1
elowanaud <elowan.audouin@icloud.com>
2026-05-30 19:59:44 +0200

    feat: import all shadcn components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { PropsWithChildren } from "react";
 import "@/globals.css";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
@@ -13,7 +15,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
       lang="en"
       className={cn("h-full antialiased", "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <TooltipProvider>{children}</TooltipProvider>
+        <Toaster />
+      </body>
     </html>
   );
 }
