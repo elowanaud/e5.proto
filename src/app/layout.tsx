fb1546686aac3c17abf77cdcbe50d80f2383b7cc
elowanaud <elowan.audouin@icloud.com>
2026-05-30 18:08:06 +0200

    feat: setup shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { PropsWithChildren } from "react";
 import "@/globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 type RootLayoutProps = Readonly<PropsWithChildren>;
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html
+      lang="en"
+      className={cn("h-full antialiased", "font-sans", inter.variable)}
+    >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
